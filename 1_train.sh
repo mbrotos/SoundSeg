3b3129fba61b5e5c5f36ec8c3fc0b7cbff514c2b
Adam Sorrenti <44556562+mbrotos@users.noreply.github.com>
2023-12-12 10:40:43 -0500

    Evaluation script

---
+++ 1_train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ./venv/bin/activate
+
+python -u train.py --normalization time --epochs 20 --batch_size 64 --quantile_scaler --loss mae --augmentations     
+python -u train.py --normalization time --epochs 20 --batch_size 64  --loss mae --augmentations
+python -u train.py --normalization frequency --epochs 20 --batch_size 64 --quantile_scaler --loss mae --augmentations
+python -u train.py --normalization frequency --epochs 20 --batch_size 64  --loss mae --augmentations
+python -u train.py --normalization time --epochs 20 --batch_size 64 --quantile_scaler --loss mse --augmentations     
+python -u train.py --normalization time --epochs 20 --batch_size 64  --loss mse --augmentations
+python -u train.py --normalization frequency --epochs 20 --batch_size 64 --quantile_scaler --loss mse --augmentations
+python -u train.py --normalization frequency --epochs 20 --batch_size 64  --loss mse --augmentations
