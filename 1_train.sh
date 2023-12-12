1876fbbe0c1f2b3931c4bc50fd260330e2dabec7
Adam Sorrenti <44556562+mbrotos@users.noreply.github.com>
2023-12-12 10:46:00 -0500

    Add unittest to train script

--- 1_train.sh
+++ 1_train.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source ./venv/bin/activate
 
+# Unit tests
+python test_audio_processing.py
+
+# Train models
 python -u train.py --normalization time --epochs 20 --batch_size 64 --quantile_scaler --loss mae --augmentations     
 python -u train.py --normalization time --epochs 20 --batch_size 64  --loss mae --augmentations
 python -u train.py --normalization frequency --epochs 20 --batch_size 64 --quantile_scaler --loss mae --augmentations
