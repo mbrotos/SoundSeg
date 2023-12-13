2a84b3cfcca33c5f19c28ee244e923edb0fb6697
Adam Sorrenti <44556562+mbrotos@users.noreply.github.com>
2023-12-13 14:12:23 -0500

    Added evaluation to demo with yt-dlp

--- src/2_eval.sh
+++ src/2_eval.sh
@@ -8,3 +8,9 @@ python run_eval.py
 
 # Alternatively, you can run the evaluation script for a single model
 # python evaluate.py --model_name '{name}'
+
+# You can download all the models with evaluation results from the following link:
+# https://drive.google.com/file/d/1_myj2HVAg-g6SR44jSRB9tIkv7BhF1gU/view?usp=sharing
+# gdown 1_myj2HVAg-g6SR44jSRB9tIkv7BhF1gU && unzip models.zip
+
+# Please see ./analysis.ipynb to create the results table in the paper
